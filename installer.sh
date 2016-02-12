338088fdbce84bb28bb826665906f659614a2ff1
danielisbetter <dangarita@montreat.edu>
2016-02-12 15:45:25 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,9 +10,10 @@ printf "
 # Welcome, This script will install scipts for you and stuff #
 ##############################################################\n\n"
 function questions() {
-read -p "Would you like to install updated sources? [y/n] " answerUpdateSources
-read -p "Do you want to download clamavinstaller? [y/n] " answerInstallClamavInstaller
-read -p "Would you like to update path? [y/n] " answerUpdatePath
+  read -p "Would you like to install updated sources? [y/n] " answerUpdateSources
+  read -p "Do you want to download clamavinstaller? [y/n] " answerInstallClamavInstaller
+  read -p "Would you like to update path? [y/n] " answerUpdatePath
+  read -p "Would you like to update SSH? [y/n] " answerUpdateSSH
 }
 questions
 
@@ -22,11 +23,15 @@ if [[ $answerUpdateSources = y ]] ; then
 fi  
 
 if [[ $answerInstallClamavInstaller = y ]] ; then
-  wget raw.githubusercontent.com/DragonDefenders/ubuntuupdater/master/clamavinstaller.sh --no-check-certificate -O /root/clamavinstaller.sh
+  wget https://raw.githubusercontent.com/DragonDefenders/ubuntuupdater/master/clamavinstaller.sh --no-check-certificate -O /root/clamavinstaller.sh
 fi
 
 if [[ $answerUpdatePath = y ]] ; then
   PATH=$PATH /sbin/
+fi
+
+if [[ $answerUpdateSSH = y ]] ; then
+  wget https://raw.githubusercontent.com/DragonDefenders/ubuntuupdater/master/sshpatcher.sh --no-check-certificate -O /root/SSHPatcher.sh
 fi
 
 function pause () {
